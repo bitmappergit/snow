4d651130aa4823d965ffa627c817f65881fcefaa
Bram Wyllie <30481775+bitmappergit@users.noreply.github.com>
2022-06-05 05:15:33 -0700

    added nomad-driver-nspawn

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages = {
+          nomad-driver-nspawn = pkgs.callPackage ./nomad-driver-nspawn.nix {};
+        };
+      }
+    );
+}
---
+++ nomad-driver-nspawn.nix
@@ -0,0 +1,27 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "nomad-driver-nspawn";
+  version = "0.9.0";
+
+  src = fetchFromGitHub {
+    owner = "JanMa";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-8owXwjQ0iPiNLXOZ9AOIDNuU5I90uca6MwRQVsExbro";
+  };
+
+  vendorSha256 = null;
+
+  subPackages = [ "." ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://www.github.com/JanMa/nomad-driver-nspawn";
+    description = "systemd-nspawn task driver for Nomad";
+    # platforms = platforms.linux;
+    license = licenses.mit;
+    maintainers = [ "bitmappergit" ];
+  };
+}
